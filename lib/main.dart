7a6bef6d7a6c078805b1b341568345287b20f879
song <songku0306@gmail.com>
2023-08-22 09:09:40 +0900

    항목제외

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:newviewprac/screens/viewone.dart';
+// import 'package:newviewprac/screens/viewone.dart';
 import 'package:newviewprac/screens/viewthree.dart';
-import 'package:newviewprac/screens/viewtwo.dart';
-import 'screens/myhomepage.dart';
+// import 'package:newviewprac/screens/viewtwo.dart';
+// import 'screens/myhomepage.dart';
 
 void main() {
   runApp(viewThree());
