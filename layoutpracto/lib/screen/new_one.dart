721e8bcf186f045f30be73e1ad62aea73d895a9f
song <songku0306@gmail.com>
2023-08-24 16:31:54 +0900

    mesm

---
+++ layoutpracto/lib/screen/new_one.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NewOne extends StatelessWidget {
+  const NewOne({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: AppBar(
+        title: const Text('My flutter'),
+      ),
+    );
+  }
+}
