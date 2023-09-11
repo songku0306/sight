95dd6e80ec785fa38aa1690f918445f2986ece20
song <songku0306@gmail.com>
2023-09-11 17:40:05 +0900

    9/11 call it a day!

--- baybaby/baybaby/lib/view/day_time.dart
+++ baybaby/baybaby/lib/view/day_time.dart
@@ -119,123 +119,152 @@ class _BabyCareScreenState extends State<BabyCareScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white38,
         title: const Text('데일리기록'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const SizedBox(height: 30), // 30픽셀의 공간 추가
+        actions: [
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              fixedSize: Size(80, 80),
+              shape: BeveledRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0), // 모서리 둥근 정도 설정
+              ),
 
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                border: Border.all(
-                  width: 8,
-                ),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: DataTable(
-                columns: const <DataColumn>[
-                  DataColumn(
-                      label: Text('기저귀',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20,
-                          ))),
-                ],
-                rows: sleepRecords.map((record) {
-                  return DataRow(
-                    color: MaterialStateProperty.resolveWith<Color?>(
-                        (Set<MaterialState> states) {
-                      // 특정 상태에 따라 행의 색상을 설정합니다.
-                      if (states.contains(MaterialState.pressed)) {
-                        return Colors.blueAccent; // 선택된 경우
-                      }
-                      return Colors
-                          .lightBlue; // 기본 색상 (null로 설정하면 테마의 색상을 사용합니다)
-                    }),
-                    cells: <DataCell>[
-                      DataCell(Text(record.date)),
-                    ],
-                  );
-                }).toList(),
+              padding: EdgeInsets.all(5), // 버튼 주위의 여백 설정
+              primary: Colors.deepOrange, // 배경색을 변경하려면 primary 속성을 사용합니다.
+            ),
+            child: const Text(
+              '마감',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
               ),
             ),
-            const SizedBox(height: 30), // 30픽셀의 공간 추가
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.green,
-                border: Border.all(
-                  width: 8,
+          ),
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              // 설정 버튼을 눌렀을 때 수행할 동작을 정의합니다.
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  border: Border.all(
+                    width: 8,
+                  ),
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: DataTable(
-                columns: const <DataColumn>[
-                  DataColumn(
-                      label: Text('분유',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20,
-                          ))),
-                ],
-                rows: feedingRecords.map((record) {
-                  return DataRow(
-                    color: MaterialStateProperty.resolveWith<Color?>(
-                        (Set<MaterialState> states) {
-                      // 특정 상태에 따라 행의 색상을 설정합니다.
-                      if (states.contains(MaterialState.pressed)) {
-                        return Colors.green; // 선택된 경우
-                      }
-                      return Colors
-                          .lightGreen; // 기본 색상 (null로 설정하면 테마의 색상을 사용합니다)
-                    }),
-                    cells: <DataCell>[
-                      DataCell(Text(record.date)),
-                    ],
-                  );
-                }).toList(),
-              ),
-            ),
-            const SizedBox(height: 30), // 30픽셀의 공간 추가
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.red,
-                border: Border.all(
-                  width: 8,
+                child: DataTable(
+                  columns: const <DataColumn>[
+                    DataColumn(
+                        label: Text('기저귀',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                            ))),
+                  ],
+                  rows: sleepRecords.map((record) {
+                    return DataRow(
+                      color: MaterialStateProperty.resolveWith<Color?>(
+                          (Set<MaterialState> states) {
+                        // 특정 상태에 따라 행의 색상을 설정합니다.
+                        if (states.contains(MaterialState.pressed)) {
+                          return Colors.blueAccent; // 선택된 경우
+                        }
+                        return Colors
+                            .lightBlue; // 기본 색상 (null로 설정하면 테마의 색상을 사용합니다)
+                      }),
+                      cells: <DataCell>[
+                        DataCell(Text(record.date)),
+                      ],
+                    );
+                  }).toList(),
                 ),
-                borderRadius: BorderRadius.circular(12),
               ),
-              child: DataTable(
-                columns: const <DataColumn>[
-                  DataColumn(
-                      label: Text('수면',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20,
-                          ))),
-                ],
-                rows: diaperRecords.map((record) {
-                  return DataRow(
-                    color: MaterialStateProperty.resolveWith<Color?>(
-                        (Set<MaterialState> states) {
-                      // 특정 상태에 따라 행의 색상을 설정합니다.
-                      if (states.contains(MaterialState.pressed)) {
-                        return Colors.redAccent; // 선택된 경우
-                      }
-                      return Colors
-                          .redAccent; // 기본 색상 (null로 설정하면 테마의 색상을 사용합니다)
-                    }),
-                    cells: <DataCell>[
-                      DataCell(Text(record.date)),
-                    ],
-                  );
-                }).toList(),
+              const SizedBox(height: 30), // 30픽셀의 공간 추가
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  border: Border.all(
+                    width: 8,
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: DataTable(
+                  columns: const <DataColumn>[
+                    DataColumn(
+                        label: Text('분유',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                            ))),
+                  ],
+                  rows: feedingRecords.map((record) {
+                    return DataRow(
+                      color: MaterialStateProperty.resolveWith<Color?>(
+                          (Set<MaterialState> states) {
+                        // 특정 상태에 따라 행의 색상을 설정합니다.
+                        if (states.contains(MaterialState.pressed)) {
+                          return Colors.green; // 선택된 경우
+                        }
+                        return Colors
+                            .lightGreen; // 기본 색상 (null로 설정하면 테마의 색상을 사용합니다)
+                      }),
+                      cells: <DataCell>[
+                        DataCell(Text(record.date)),
+                      ],
+                    );
+                  }).toList(),
+                ),
               ),
-            ),
-            const SizedBox(height: 30),
-          ],
+              const SizedBox(height: 30), // 30픽셀의 공간 추가
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  border: Border.all(
+                    width: 8,
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: DataTable(
+                  columns: const <DataColumn>[
+                    DataColumn(
+                        label: Text('수면',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                            ))),
+                  ],
+                  rows: diaperRecords.map((record) {
+                    return DataRow(
+                      color: MaterialStateProperty.resolveWith<Color?>(
+                          (Set<MaterialState> states) {
+                        // 특정 상태에 따라 행의 색상을 설정합니다.
+                        if (states.contains(MaterialState.pressed)) {
+                          return Colors.redAccent; // 선택된 경우
+                        }
+                        return Colors
+                            .redAccent; // 기본 색상 (null로 설정하면 테마의 색상을 사용합니다)
+                      }),
+                      cells: <DataCell>[
+                        DataCell(Text(record.date)),
+                      ],
+                    );
+                  }).toList(),
+                ),
+              ),
+              const SizedBox(height: 30),
+            ],
+          ),
         ),
       ),
       floatingActionButton: Row(
