46824737359454139f68b339e287990c98a1d7b6
song <songku0306@gmail.com>
2023-09-12 09:32:42 +0900

    9/12 day edited

--- baybaby/baybaby/lib/view/day_time.dart
+++ baybaby/baybaby/lib/view/day_time.dart
@@ -66,24 +66,6 @@ class _BabyCareScreenState extends State<BabyCareScreen> {
             ),
           ),
         );
-      } else if (type == 'Feeding') {
-        feedingRecords.insert(0, record);
-        // 스위치 동작 시 '분유' ElevatedButton 추가
-        feedingRecordsButton.add(
-          ElevatedButton(
-            onPressed: () => navigateToDetailScreen(feedingRecords),
-            style: ElevatedButton.styleFrom(
-              primary: Colors.green,
-            ),
-            child: Text(
-              '분유',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        );
       } else if (type == 'Sleep') {
         sleepRecords.insert(0, record);
         // 스위치 동작 시 '기저귀' ElevatedButton 추가
@@ -115,6 +97,62 @@ class _BabyCareScreenState extends State<BabyCareScreen> {
     );
   }
 
+  void addFeeding() {
+    String newFeedingText = ''; // 사용자로부터 입력받은 문자열을 저장할 변수
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('새로운 분유 추가'), // 대화 상자의 제목
+          content: TextField(
+            onChanged: (value) {
+              newFeedingText = value; // TextField의 값이 변경될 때마다 변수에 저장
+            },
+            decoration: InputDecoration(
+              hintText: '분유 내용을 입력하세요', // 입력 힌트
+            ),
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                if (newFeedingText.isNotEmpty) {
+                  setState(() {
+                    // 스위치 동작 시 '분유' ElevatedButton 추가
+                    feedingRecordsButton.add(
+                      ElevatedButton(
+                        onPressed: () => navigateToDetailScreen(feedingRecords),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.green,
+                        ),
+                        child: Text(
+                          '분유',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    );
+                  });
+
+                  Navigator.of(context).pop(); // 대화 상자 닫기
+                }
+              },
+              child: Text('추가'), // 추가 버튼
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // 취소 버튼
+              },
+              child: Text('취소'), // 취소 버튼
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -141,12 +179,9 @@ class _BabyCareScreenState extends State<BabyCareScreen> {
               ),
             ),
           ),
-          IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: () {
-              // 설정 버튼을 눌렀을 때 수행할 동작을 정의합니다.
-            },
-          ),
+          SizedBox(
+            width: 40,
+          )
         ],
       ),
       body: Padding(
@@ -192,6 +227,7 @@ class _BabyCareScreenState extends State<BabyCareScreen> {
               ),
               const SizedBox(height: 30), // 30픽셀의 공간 추가
               Container(
+                width: double.infinity,
                 decoration: BoxDecoration(
                   color: Colors.green,
                   border: Border.all(
@@ -199,31 +235,22 @@ class _BabyCareScreenState extends State<BabyCareScreen> {
                   ),
                   borderRadius: BorderRadius.circular(12),
                 ),
-                child: DataTable(
-                  columns: const <DataColumn>[
-                    DataColumn(
-                        label: Text('분유',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20,
-                            ))),
-                  ],
-                  rows: feedingRecords.map((record) {
-                    return DataRow(
-                      color: MaterialStateProperty.resolveWith<Color?>(
-                          (Set<MaterialState> states) {
-                        // 특정 상태에 따라 행의 색상을 설정합니다.
-                        if (states.contains(MaterialState.pressed)) {
-                          return Colors.green; // 선택된 경우
-                        }
-                        return Colors
-                            .lightGreen; // 기본 색상 (null로 설정하면 테마의 색상을 사용합니다)
-                      }),
-                      cells: <DataCell>[
-                        DataCell(Text(record.date)),
-                      ],
-                    );
-                  }).toList(),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: 20,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('분유',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                          )),
+                      Text('분유11'),
+                    ],
+                  ),
                 ),
               ),
               const SizedBox(height: 30), // 30픽셀의 공간 추가
