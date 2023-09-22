da4073a6dbf4258fc4eb7fb00d381839f85b40be
song <songku0306@gmail.com>
2023-09-22 17:13:23 +0900

    9/22 why errors

--- apipractice/lib/main.dart
+++ apipractice/lib/main.dart
@@ -1,106 +1,150 @@
 import 'dart:convert';
- 
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
- 
+
 void main() {
-  runApp(const MyApp());
+  runApp(NursingRoomApp());
 }
- 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
- 
+
+class NursingRoomApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const TodosWidget(),
+      title: 'Nursing Room App',
+      home: NursingRoomScreen(),
     );
   }
 }
- 
-class Todos {
-  int? userId;
-  int? id;
-  String? title;
-  bool? completed;
- 
-  Todos({this.userId, this.id, this.title, this.completed});
- 
-  Todos.fromJson(Map<String, dynamic> json) {
-    userId = json['userId'];
-    id = json['id'];
-    title = json['title'];
-    completed = json['completed'];
-  }
- 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['userId'] = this.userId;
-    data['id'] = this.id;
-    data['title'] = this.title;
-    data['completed'] = this.completed;
-    return data;
-  }
-}
- 
-Future<List<Todos>> fetchTodos() async {
-  final response = await http.get(
-      Uri.parse('https://jsonplaceholder.typicode.com/todos')
-  );
- 
-  if(response.statusCode == 200){
-    return (jsonDecode(response.body) as List)
-        .map((e) => Todos.fromJson(e))
-        .toList();
-  } else {
-    throw Exception('Failed to load album');
-  }
-}
- 
-class TodosWidget extends StatefulWidget {
-  const TodosWidget({Key? key}) : super(key: key);
- 
+
+class NursingRoomScreen extends StatefulWidget {
   @override
-  _TodosWidgetState createState() => _TodosWidgetState();
+  _NursingRoomScreenState createState() => _NursingRoomScreenState();
 }
- 
-class _TodosWidgetState extends State<TodosWidget> {
-  late Future<List<Todos>> futureTodos;
- 
+
+class _NursingRoomScreenState extends State<NursingRoomScreen> {
+  String serviceKey = 'a3GptWb07Pi1Gxv7GDsZ195JQT%2BehIA65OSl04QTsSyaxeTIMA6Y7ZMOa9tIv7ywXzaqW5lWgpU4fjoRTT1lDA%3D%3D';
+  String apiUrl =
+      'http://apis.data.go.kr/6260000/BusanNursingroomInfoService/getNursingroomInfo?serviceKey=$serviceKey&numOfRows=10&pageNo=1';
+
+  bool isLoading = false;
+  List<NursingRoomInfo> nursingRooms = [];
+
   @override
   void initState() {
     super.initState();
-    futureTodos = fetchTodos();
+    fetchData();
   }
- 
+
+  Future<void> fetchData() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        final List<dynamic> items =
+            jsonData['getNursingroomInfo']['body']['items']['item'];
+
+        nursingRooms = items.map((item) {
+          return NursingRoomInfo(
+            sido: item['sido'],
+            sigungu: item['sigungu'],
+            sj: item['sj'],
+            address: item['address'],
+            place: item['place'],
+            tel: item['tel'],
+            target: item['target'],
+            father: item['father'],
+            lng: double.parse(item['lng']),
+            lat: double.parse(item['lat']),
+            confirmDate: item['confirm_date'],
+          );
+        }).toList();
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Singl(
-      child: FutureBuilder<List<Todos>>(
-          future: futureTodos,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Column(
-                children: <Widget>[
-                  ...snapshot.data!.map((e) => SizedBox(
-                    width: double.infinity,
-                    child: Card(
-                      elevation: 4,
-                      child: Text(e.title!),
-                    ),
-                  )),
-                ],
-              );
-            } else if(snapshot.hasError) {
-              return Text('${snapshot.hasError}');
-            }
-            return const CircularProgressIndicator();
-          }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Nursing Room'),
+      ),
+      body: Center(
+        child:
+            isLoading ? CircularProgressIndicator() : NursingRoomList(nursingRooms),
       ),
     );
   }
+}
+
+class NursingRoomInfo {
+  final String? sido;
+  final String? sigungu;
+  final String? sj;
+  final String? address;
+  final String? place;
+  final String? tel;
+  final String? target;
+  final String? father;
+  final double lng;
+  final double lat;
+  final String? confirmDate;
+
+  NursingRoomInfo({
+    this.sido,
+    this.sigungu,
+    this.sj,
+    this.address,
+    this.place,
+    this.tel,
+    this.target,
+    this.father,
+    required this.lng,
+    required this.lat,
+    this.confirmDate,
+});
+
+class NursingRoomList extends StatelessWidget {
+final List<NursingRoomInfo> nursingRooms;
+
+NursingRoomList({required this.nursingRooms});
+
+@override
+Widget build(BuildContext context) {
+return ListView.builder(
+itemCount: nursingRooms.length,
+itemBuilder:(context, index){
+final room = nursingRooms[index];
+return ListTile(
+title : Text(room.sj ?? ''),
+subtitle : Column(
+crossAxisAlignment: CrossAxisAlignment.start,
+children: [
+Text('Address: ${room.address ?? ''}'),
+Text('Tel: ${room.tel ?? ''}'),
+Text('Place: ${room.place ?? ''}'),
+Text('Sido: ${room.sido ?? ''}'),
+Text('Sigungu: ${room.sigungu ?? ''}'),
+Text('Target: ${room.target ?? ''}'),
+Text('Father: ${room.father ?? ''}'),
+Text('Confirm Date:${room.confirmDate??''}')
+],
+),
+);
+},
+);
+}
 }
