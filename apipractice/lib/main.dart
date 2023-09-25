c3607ee6577fd06f68f7ceed2a50164cdbf159cb
song <songku0306@gmail.com>
2023-09-25 15:45:54 +0900

    9/25 api toast msg

--- apipractice/lib/main.dart
+++ apipractice/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:xml/xml.dart' as xml;
 import 'package:xml2json/xml2json.dart';
 import 'dart:convert';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(NursingRoomApp());
@@ -207,22 +208,27 @@ class NursingRoomList extends StatelessWidget {
       itemCount: nursingRooms.length,
       itemBuilder: (context, index) {
         final item = nursingRooms[index];
-        return ListTile(
-          title: Text('이름: ${item.sj}'),
-          subtitle: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('주소: ${item.address}'),
-              Text('전화번호: ${item.tel}'),
-              Text('장소명: ${item.place}'),
-              Text('시도: ${item.sido}'),
-              Text('시군구: ${item.sigungu}'),
-              Text('타겟: ${item.target}'),
-              Text('아빠: ${item.father}'),
-              Text('확인일: ${item.confirmDate}'),
-              Text('위도: ${item.lng}'),
-              Text('경도: ${item.lat}'),
-            ],
+        return GestureDetector(
+          onTap: () {
+            // 리스트 타일을 터치할 때 토스트 메시지로 정보 표시
+            final message =
+                '이름: ${item.sj}\n장소명: ${item.place}\n주소: ${item.address}\n아빠: ${item.father}\n확인일: ${item.confirmDate}';
+            Fluttertoast.showToast(
+              msg: message,
+              toastLength: Toast.LENGTH_LONG,
+            );
+          },
+          child: ListTile(
+            title: Text('이름: ${item.sj}'),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('장소명: ${item.place}'),
+                Text('주소: ${item.address}'),
+                Text('아빠: ${item.father}'),
+                Text('확인일: ${item.confirmDate}'),
+              ],
+            ),
           ),
         );
       },
