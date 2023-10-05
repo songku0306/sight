c58ee4dc241e10cc89291b9c12ea32a97a5fa97d
song <songku0306@gmail.com>
2023-10-05 17:49:34 +0900

    10/5 apipractice cvs try

---
+++ apioffline/lib/nursingview.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+class NursingRoomApp extends StatelessWidget {
+  const NursingRoomApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nursing Room App',
+      home: NursingRoomScreen(),
+    );
+  }
+}
+
+class NursingRoomScreen extends StatefulWidget {
+  @override
+  _NursingRoomScreenState createState() => _NursingRoomScreenState();
+}
+
+class _NursingRoomScreenState extends State<NursingRoomScreen> {
+   
+  @override
+  void initState() {
+    super.initState();
+     }
+      @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              '부산 수유실',
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            if (currentPosition != null) // 현재 위치가 있을 때만 표시
+              Text(
+                '현재 위치: {currentPosition!.latitude}, {currentPosition!.longitude}',
+                style: TextStyle(
+                  fontSize: 12, // 텍스트 크기 조정
+                  color: Colors.grey, // 텍스트 색상 설정
+                ),
+              ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: isLoading
+            ? CircularProgressIndicator()
+            : nursingRooms.isNotEmpty
+                ? NursingRoomList(
+                    nursingRooms: nursingRooms,
+                    currentPosition: currentPosition,
+                  )
+                : Text('데이터가 없습니다.'), // 데이터가 없는 경우에 메시지 출력
+      ),
+    );
+  }
+}
