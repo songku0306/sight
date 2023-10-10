37684d4aa1982b5d6e7026067b22817a276aea13
song <songku0306@gmail.com>
2023-10-10 17:45:38 +0900

    10/10 eclipse

--- apioffline/lib/units.dart
+++ apioffline/lib/units.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class NursingFrame extends StatelessWidget {
   const NursingFrame({
     super.key,
@@ -9,9 +8,10 @@ class NursingFrame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 130,
+      height: 150,
       width: double.infinity,
       decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15), //모서리를 둥글게
         color: Colors.white,
       ),
       child: Padding(
@@ -21,15 +21,23 @@ class NursingFrame extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('장소명', style: TextStyle(fontSize: 30)),
+                Expanded(
+                  child: Text(
+                    '사하구건강가정지원센터공동육아나눔터',
+                    style: TextStyle(fontSize: 30),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
                 Text('확인일:2020/12/30'),
               ],
             ),
             Row(
               children: [
                 Text('주소 : ', style: TextStyle(fontSize: 20)),
-                Text('주소 / '),
-                Text('위치'),
+                Expanded(
+                    child: Text(
+                        '부산 해운대구 해운대로 지하 396 (우동, 부산지하철 시립미술관역)' + '고객센터 옆')),
               ],
             ),
             Row(
